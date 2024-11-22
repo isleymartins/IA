36ccedb1d84b926f7dbb419e7cf0143f3b760b01
isleymartinsbarbosa@gmail.com <irllamartinsbarbosa@gmail.com>
2024-11-22 10:11:04 -0300

    Layout totalmente mudado no front, acrescimo de authcontext(Ainda com ajustes). Abra para as metricas de qualidade. Bug ao trocar aba provavelmente por muita requisicoes. Tentar resolver olhando a quantidade de rederização e a sua relação com o contexto alem de problema de tipagem

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -4,8 +4,10 @@ import { Form,ModelPrediction } from "../model/model";
 interface AuthContextType {
   formData: Form ;
   setFormData: (formData: Form) => void;
+  
   directory: string[];
   setDirectory: (directory: string[]) => void;
+ 
   modelPrediction: ModelPrediction[];
   setModelPrediction: (modelPrediction: ModelPrediction[]) => void;
 }
